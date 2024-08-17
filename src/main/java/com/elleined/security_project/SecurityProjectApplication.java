e3075bf918e49022b27c397bbe5abc0976bcb701
Elleined <demadegu@gmail.com>
2024-08-18 07:17:13 +0800

    Initial commit

---
+++ src/main/java/com/elleined/security_project/SecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.elleined.security_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elleined/security_project/SecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elleined.security_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
