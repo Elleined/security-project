449d1e8f9d840f77b48e1c85e34631bcace2c5b7
Elleined <demadegu@gmail.com>
2024-08-18 07:23:47 +0800

    COnfigured configurations of pom.xml and application properties

--- src/main/java/com/elleined/security_project/SecurityProjectApplication.java
+++ src/main/java/com/elleined/security_project/SecurityProjectApplication.java
@@ -2,8 +2,14 @@ package com.elleined.security_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class SecurityProjectApplication {
 
 	public static void main(String[] args) {
