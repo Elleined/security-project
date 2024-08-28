b3703e7e5d06814485436162395f526ece4b125c
Elleined <demadegu@gmail.com>
2024-08-28 18:06:27 +0800

    Added role and permission controller

--- src/main/java/com/elleined/security_project/controller/PermissionController.java
+++ src/main/java/com/elleined/security_project/controller/PermissionController.java
@@ -1,4 +1,53 @@
 package com.elleined.security_project.controller;
 
+import com.elleined.security_project.model.Permission;
+import com.elleined.security_project.model.User;
+import com.elleined.security_project.repository.PermissionRepository;
+import com.elleined.security_project.repository.UserRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/permissions")
+@RequiredArgsConstructor
 public class PermissionController {
+
+    private final PermissionRepository permissionRepository;
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public Permission getById(@PathVariable("id") int id) {
+        return permissionRepository.findById(id).orElseThrow(() -> new RuntimeException("Permission with id of "));
+    }
+
+    @GetMapping
+    public List<Permission> getAll() {
+        return permissionRepository.findAll();
+    }
+
+    @PatchMapping("/{id}")
+    public void addPermission(@PathVariable("id") int id,
+                              @RequestParam("userId") int userId) {
+
+        User user = userRepository.findById(userId).orElseThrow();
+        Permission permission = permissionRepository.findById(id).orElseThrow();
+
+        user.getPermissions().add(permission);
+        userRepository.save(user);
+    }
+
+    @PatchMapping("/{id}")
+    public void removePermission(@PathVariable("id") int id,
+                                 @RequestParam("userId") int userId) {
+
+        User user = userRepository.findById(userId).orElseThrow();
+        Permission permission = permissionRepository.findById(id).orElseThrow();
+
+        user.getPermissions().remove(permission);
+        userRepository.save(user);
+    }
 }
--- src/main/java/com/elleined/security_project/controller/RoleController.java
+++ src/main/java/com/elleined/security_project/controller/RoleController.java
@@ -1,12 +1,51 @@
 package com.elleined.security_project.controller;
 
+import com.elleined.security_project.model.Role;
+import com.elleined.security_project.model.User;
+import com.elleined.security_project.repository.RoleRepository;
+import com.elleined.security_project.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/role-authorization")
+@RequestMapping("/roles")
 @RequiredArgsConstructor
 public class RoleController {
+    private final RoleRepository roleRepository;
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public Role getById(int id) {
+        return roleRepository.findById(id).orElseThrow(() -> new RuntimeException("Role with id of "));
+    }
+
+    @GetMapping
+    public List<Role> getAll() {
+        return roleRepository.findAll();
+    }
+
+    @PatchMapping("/{id}")
+    public void addRole(@PathVariable("id") int id,
+                        @RequestParam("userId") int userId) {
+
+        User user = userRepository.findById(userId).orElseThrow();
+        Role role = roleRepository.findById(id).orElseThrow();
+        user.getRoles().add(role);
+
+        userRepository.save(user);
+    }
+
+    @PatchMapping("/{id}")
+    public void removeRole(@PathVariable("id") int id,
+                           @RequestParam("userId") int userId) {
+
+        User user = userRepository.findById(userId).orElseThrow();
+        Role role = roleRepository.findById(id).orElseThrow();
+        user.getRoles().remove(role);
+
+        userRepository.save(user);
+    }
 }
